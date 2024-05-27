0251bf6686015313bcd5c89d6d01a850a4da750d
muhammadffa <muhammaddaffa463@gmail.com>
2024-05-27 21:13:08 +0700

    update env

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+load_dotenv()
 
 
 
